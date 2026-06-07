ee2d34665bff9f963f60eb043a53823814b5f591
Diego RA <diego@qosit.com.br>
2026-06-07 17:18:30 -0300

    feat(bot): implement queue routing and confirmation messages

--- src/application/messages/confirmation-messages.ts
+++ src/application/messages/confirmation-messages.ts
@@ -1,5 +1,3 @@
-// src/application/messages/confirmation-messages.ts
-
 export const SUPPORT_CONFIRMATION_MESSAGE =
   "Só um momento enquanto transfiro pra nosso time.\n\nPara adiantar o seu atendimento, nos informe o nome da sua Empresa ou CNPJ:";
 
@@ -8,3 +6,9 @@ export const FINANCE_CONFIRMATION_MESSAGE =
 
 export const OTHER_CONFIRMATION_MESSAGE =
   "Só um momento enquanto transfiro pra nosso time.\n\nPara adiantar o seu atendimento, nos informe o nome da sua Empresa ou CNPJ:";
+
+export const AFTER_HOURS_SUPPORT_CONFIRMATION_MESSAGE =
+  "Recebemos sua solicitação e ela foi encaminhada para nosso time.\n\nNosso horário de atendimento, *neste canal*, é de segunda a sexta-feira, das 08:30 às 12:00 e das 13:00 às 17:30.\n\nCaso se trate de uma emergência, pedimos que entre em contato pelo telefone (41) 4063-7066.\n\nCaso contrário, responderemos no próximo horário útil.";
+
+export const AFTER_HOURS_OTHER_CONFIRMATION_MESSAGE =
+  "Recebemos sua solicitação e ela foi encaminhada para nosso time.\n\nNosso horário de atendimento, *neste canal*, é de segunda a sexta-feira, das 08:30 às 12:00 e das 13:00 às 17:30.\n\nResponderemos no próximo horário útil.";
